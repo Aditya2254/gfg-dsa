5eaae9ef08b8f5523d1016c9f62cd2fae741d069
Aditya Raj <aditya1.raj@aot.edu.in>
2022-03-20 21:30:38 +0530

    .

---
+++ ch-6/searchInSortedRotatedArr.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+int search(int arr[], int n, int k)
+{
+    int low = 0, high = n - 1;
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (arr[mid] == k)
+            return mid;
+        if (arr[low] < arr[mid])
+        {
+            if (k >= arr[low] && k <= arr[mid])
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        else
+        {
+            if (k >= arr[mid] && k <= arr[high])
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+    }
+    return -1;
+}
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << search(arr, n, k);
+    return 0;
+}
